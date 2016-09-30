575c15674718041a45c9735f90db05b7c218e668
WeizhongTu <tuweizhong@163.com>
2016-09-30 12:00:15 +0800

    add tldr and tmux

--- run.sh
+++ run.sh
@@ -80,3 +80,10 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 cd ~/.vim_runtime/sources_non_forked
 git clone https://github.com/davidhalter/jedi-vim.git
 
+# install tmux
+sudo yum install tmux
+
+# too long, don't read!
+sudo pip install tldr
+# now you can use `tldr tmux` to see how to use tmux
+
