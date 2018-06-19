d180bded884884c18efd03ee7fb18cd416af68c3
WeizhongTu <tuweizhong@163.com>
2018-06-19 21:22:51 +0800

    Update timed_rotate_log_example.py

--- python/timed_rotate_log_example.py
+++ python/timed_rotate_log_example.py
@@ -1,14 +1,37 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import logging
+import sys
 from logging.handlers import TimedRotatingFileHandler
+FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+LOG_FILE = "my-app.log"
 
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
 
-logger = logging.getLogger(__name__)
-formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
-handler = TimedRotatingFileHandler(filename='/Users/tu/testlog.log', when='D')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
 
-logger.debug('log content')
+
+def get_file_handler(file_path=None):
+    if not file_path:
+       file_path = LOG_FILE
+    file_handler = TimedRotatingFileHandler(file_path, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    # better to have too much log than not enough
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+    # it's rarely necessary to propagate the error up to parent
+    logger.propagate = False
+    return logger
+
+
+if __name__ == '__main__':
+    logger = get_logger('zqxt')
+    logger.info('just a test')
+    logger.debug('ok')
